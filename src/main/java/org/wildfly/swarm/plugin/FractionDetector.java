56450a1c2bda46916b5dd0d0f006d3b4396651ac
Bob McWhirter <bob@mcwhirter.org>
2016-08-16 16:08:49 -0400

    Generate META-INF/fraction.properties for each fraction. (#9)
    Provide handy-dandy access, per-fraction, of
    
     * name
     * groupId
     * artifactId
     * version
     * stabilityLevel (a word)
     * stabilityIndex (a number)

---
+++ src/main/java/org/wildfly/swarm/plugin/FractionDetector.java
@@ -0,0 +1,20 @@
+package org.wildfly.swarm.plugin;
+
+import org.apache.maven.project.MavenProject;
+
+import static org.wildfly.swarm.plugin.AbstractFractionsMojo.FRACTION_INTERNAL_PROPERTY_NAME;
+import static org.wildfly.swarm.plugin.AbstractFractionsMojo.FRACTION_TAGS_PROPERTY_NAME;
+import static org.wildfly.swarm.plugin.StabilityLevel.FRACTION_STABILITY_PROPERTY_NAME;
+
+/**
+ * @author Bob McWhirter
+ */
+public class FractionDetector {
+
+    public static boolean isFraction(MavenProject project) {
+        boolean result = project.getProperties().getProperty(FRACTION_STABILITY_PROPERTY_NAME) != null
+                || project.getProperties().getProperty(FRACTION_TAGS_PROPERTY_NAME) != null
+                || project.getProperties().getProperty(FRACTION_INTERNAL_PROPERTY_NAME ) != null;
+        return result;
+    }
+}
--- src/main/java/org/wildfly/swarm/plugin/StabilityLevel.java
+++ src/main/java/org/wildfly/swarm/plugin/StabilityLevel.java
@@ -1,5 +1,9 @@
 package org.wildfly.swarm.plugin;
 
+import java.util.Properties;
+
+import org.apache.maven.project.MavenProject;
+
 /**
  * @author Bob McWhirter
  */
@@ -12,6 +16,10 @@ public enum StabilityLevel {
     FROZEN,
     LOCKED;
 
+    protected static final String DEFAULT_STABILITY_INDEX = "unstable";
+
+    protected static final String FRACTION_STABILITY_PROPERTY_NAME = "swarm.fraction.stability";
+
     public static StabilityLevel parse(String level) {
         level = level.toUpperCase().trim();
         try {
@@ -21,5 +29,10 @@ public enum StabilityLevel {
             // oh well, try it as a word.
         }
         return StabilityLevel.valueOf(level);
+    }
+
+    public static StabilityLevel of(MavenProject project) {
+        Properties properties = project.getProperties();
+        return StabilityLevel.parse(properties.getProperty(FRACTION_STABILITY_PROPERTY_NAME, DEFAULT_STABILITY_INDEX));
     }
 }
